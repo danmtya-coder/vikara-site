2cfa1307a38addffdbc778add7df4f422d923ac3
danmtya-coder <danmtya-coder@users.noreply.github.com>
2026-06-20 11:48:24 +0000

    deploy: danmtya-coder/vikara-app@b280eb39e347f06776f3be39783af83ee630e0b4

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2422146568"
+    serviceWorkerVersion: "4136616857"
   }
 });
